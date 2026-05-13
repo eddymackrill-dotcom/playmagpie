aec95469f0008fe77b35c15320217c67332a1639
Eddy Mackrill <eddymackrill@googlemail.com>
2026-05-13 11:17:08 +0100

    Add Google Analytics G-9ZJ8WY3PS8

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: {
@@ -43,6 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         <main>{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-9ZJ8WY3PS8" />
       </body>
     </html>
   )
